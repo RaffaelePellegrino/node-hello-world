e2d7af2d397630fd43610f5996937500679808fc
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2024-11-28 20:06:03 +0100

    nome passato da terminale

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+let parola_passata = process.argv[2]
+
+console.log("Hello " + parola_passata)
+
+console.log("ciao ciao ciao")
+
+// node ScriptProcessorNode.js Raffaele
